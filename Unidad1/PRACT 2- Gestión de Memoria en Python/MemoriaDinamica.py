27a32a3dd9688a7aeaa00e5e1a527300d73ba5b2
Jose Chac Chay <josechacchay95@gmail.com>
2026-02-09 13:05:54 -0600

    MemoriaDinamica
    Este programa en Python trabaja con una lista llamada frutas, la cual inicia vacía. Posteriormente, se agregan elementos a la lista utilizando el método append, incorporando las frutas Mango, Manzana, Banana y Uvas. Después de mostrar el contenido inicial de la lista en pantalla, se eliminan elementos específicos usando el método pop, primero eliminando la fruta ubicada en la posición cero y luego la que queda en la posición uno. Finalmente, se agrega una nueva fruta, Sandía, y se imprime nuevamente la lista, mostrando el resultado final tras las modificaciones realizadas.

---
+++ Unidad1/PRACT 2- Gestión de Memoria en Python/MemoriaDinamica.py
@@ -0,0 +1,12 @@
+frutas = []
+
+frutas.append("Mango")
+frutas.append("Manzana")
+frutas.append("Banana")
+frutas.append("Uvas")
+print(frutas)
+frutas.pop(0)
+frutas.pop(1)
+frutas.append("Sandia")
+
+print(frutas)
